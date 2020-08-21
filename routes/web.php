da0d5e0b6a99473dd228c0fe0d26c9a51d474f7c
liuzhen <2932992895@qq.com>
2020-08-21 20:28:54 +0800

    登录

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,23 @@
 |
 */
 
+
+Route::any('/', function () {
+    return view('welcome');
+});
+//发送验证码
+Route::any('/vicode','api\LoginController@vicode');
+
+Route::any('/login','api\LoginController@login');
+
+Route::any('/test','api\LoginController@test');
+
+
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::any('/','api\NewsController@index');
+
+Route::any('/' , 'IndexController@index' );
+
+Route::any('/testa','api\LoginController@test');
+
