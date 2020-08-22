44e255dd8e8aa29cc749be71c9a4b1c70c43c0e0
zhaoyi <1113252039@qq.com>
2020-08-22 10:00:49 +0800

    3

--- app/models/NewsModel.php
+++ app/models/NewsModel.php
@@ -3,14 +3,13 @@
 namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
-
 class NewsModel extends Model
 {
     public $table="news_news";
     public $primaryKey='news_id';
     public $timestamps=false;
     public function getCate(){
-        return $this->hasOne('App\models\CateModel','cate_id','cate_id');
+        return $this->hasOne('App\models\CateModel','cate_id','cate_name');
     }
 }
 
